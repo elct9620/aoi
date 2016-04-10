d5c490337270da85db45e13296be92cf91046063
蒼時弦也 <elct9620@frost.tw>
2016-04-10 21:19:41 +0800

    Improve command options

--- bin/index.js
+++ bin/index.js
@@ -14,28 +14,26 @@ let pkg = require('../package.json');
 let watchCommand = require('../commands/watch');
 let buildCommand = require('../commands/build');
 
+function sharedOptions(program) {
+    program
+        .option('-e, --entry [entry]', 'The entry file, defaule is src/index.js')
+        .option('-o, --output [output]', 'Output filename, default is bundle.js')
+        .option('-O, --style-output [styleOutput]', 'The extract style file dest, default is style.css')
+        .option('-d, --dest [dest]', 'Destination of generated file, default is ./build')
+        .option('-p, --public [publicPath]', 'Public path, defaut is ./');
+
+    return program;
+}
+
 program
     .version(pkg.version);
 
-program
-    .command('build')
+sharedOptions(program.command('build'))
     .description('Generate static assets')
-    .option('-e, --entry [entry]', 'The entry file, defaule is src/index.js')
-    .option('-o, --output [output]', 'Output filename, default is bundle.js')
-    .option('-d, --dest [dest]', 'Generated file destination, default is ./build')
-    .option('-so, --style-output [styleOutput]', 'The extract style file dest, default is style.css')
-    .option('-p, --public [publicPath]', 'Public path, defaut is ./')
     .action(buildCommand);
 
-program
-    .command('watch')
+sharedOptions(program.command('watch'))
     .description('Watch the working directory and build assets in realtime')
-    .option('-s, --serve [port]', 'Run built-in web server with specify port')
-    .option('-e, --entry [entry]', 'The entry file, defaule is src/index.js')
-    .option('-o, --output [output]', 'Output filename, default is bundle.js')
-    .option('-d, --dest [dest]', 'Generated file destination, default is ./build')
-    .option('-so, --style-output [styleOutput]', 'The extract style file dest, default is style.css')
-    .option('-p, --public [publicPath]', 'Public path, defaut is ./')
     .action(watchCommand);
 
 program.parse(process.argv);
